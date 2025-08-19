6de91576b55b862de64ae2c1bd342ea9ed447bae
Harsh <kaushikyuvraj134@gmail.com>
2025-08-19 18:33:31 +0530

    add parallex background

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import HeroText from '../components/HeroText'
+import ParallexBackground from '../components/ParallexBackground'
 
 const Hero = () => {
   return (
     <section className='flex items-start justify-center ms:items-start md:justify-start mih-h-screen overflow-hidden c-space'>
         <HeroText/>
+        <ParallexBackground/>
+        <figure className='absolute inset-0' style={{width: "100vw", height: "100vh"}}>
+
+        </figure>
     </section>
   )
 }
