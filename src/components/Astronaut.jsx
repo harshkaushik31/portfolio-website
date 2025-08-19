b28f164bd263d05de236eab3e87b11f5aee10bde
Harsh <kaushikyuvraj134@gmail.com>
2025-08-19 19:37:17 +0530

    hero section complete

--- src/components/Astronaut.jsx
+++ src/components/Astronaut.jsx
@@ -8,6 +8,8 @@ Title: Tenhun Falling spaceman (FanArt)
 
 import React, { act, useEffect, useRef } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
+import { useMotionValue, useSpring } from 'motion/react';
+import { useFrame } from '@react-three/fiber';
 
 export function Astronaut(props) {
   const group = useRef()
@@ -18,6 +20,19 @@ export function Astronaut(props) {
         actions[animations[0].name]?.play();
     }
   },[actions, animations])
+
+  const yPosition = useMotionValue(5);
+  const ySpring = useSpring(yPosition, {damping: 30})
+
+  useEffect(()=>{
+    ySpring.set(-1);
+  },[ySpring])
+
+  useFrame(()=>{
+    group.current.position.y = ySpring.get();
+  })
+
+
   return (
     <group ref={group} {...props} dispose={null} rotation={[-Math.PI / 2, -0.2, 2.2]} scale={props.scale ||  0.3}
     position={props.position || [1.3, -1, 0 ]}>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,15 +1,17 @@
-import { Canvas } from "@react-three/fiber";
-import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense } from "react";
 import HeroText from "../components/HeroText";
 import ParallexBackground from "../components/ParallexBackground";
 import { Astronaut } from "../components/Astronaut";
-import { OrbitControls } from "@react-three/drei";
+import { Float, OrbitControls } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
+import Loader from "../components/Loader";
+import { easing } from 'maath'
 
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
-    <section className="flex items-start justify-center ms:items-start md:justify-start mih-h-screen overflow-hidden c-space">
+    <section className="flex items-start justify-center sm:items-start md:justify-start mih-h-screen overflow-hidden c-space">
       <HeroText />
       <ParallexBackground />
       <figure
@@ -17,19 +19,37 @@ const Hero = () => {
         style={{ width: "100vw", height: "100vh" }}
       >
         <Canvas camera={{ position: [0, 1, 3] }}>
-          <Astronaut
-            scale={isMobile && 0.23}
-            position={isMobile && [0, -1.5, 0]}
-          />
-          <OrbitControls
+          <Suspense fallback={ <Loader/>}>
+            <Float>
+              <Astronaut
+                scale={isMobile && 0.23}
+                position={isMobile && [0, -1.5, 0]}
+              />
+            </Float>
+
+            {/* <OrbitControls
             enableZoom={false}
             enablePan={false}
             enableRotate={true}
-          />
+            /> */}
+            <Rig />
+          </Suspense>
         </Canvas>
       </figure>
     </section>
   );
 };
+
+function Rig() {
+  useFrame((state, delta) => {
+    easing.damp3(
+      state.camera.position,
+      [state.mouse.x / 10, 1 + state.mouse.y / 10, 3],
+      0.5,
+      delta
+    );
+  });
+  return null; 
+}
 
 export default Hero;
